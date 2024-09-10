fe4075c9c18a37d8fb8fc9092a1ab6676cd19a31
RoMango <112846459+FlewToL@users.noreply.github.com>
2024-09-11 01:18:37 +0300

    Add files via upload

--- SecondApp/main.dart
+++ SecondApp/main.dart
@@ -1,1 +1,173 @@
-1
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SecondApp());
+}
+
+class SecondApp extends StatelessWidget {
+  const SecondApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: LoginForm(),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        const Positioned(
+          top: 100,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Text(
+              'Авторизация',
+              style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 40),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color(0xffe9ecef),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  labelText: 'Логин',
+                  labelStyle: const TextStyle(
+                    color: Color(0xE79396A5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color(0xffe7eaed),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  labelText: 'Пароль',
+                  labelStyle: const TextStyle(
+                    color: Color(0xE79396A5),
+                    fontSize: 18,
+                  ),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Checkbox(
+                    value: false,
+                    onChanged: (bool? value) {},
+                    checkColor: const Color(0xFF3E3E3E),
+                  ),
+                  const Text(
+                    'Запомнить меня',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF656565)
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 56,
+                width: 380,
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'Войти',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 56,
+                width: 380,
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.blue, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'Регистрация',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Восстановить пароль',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                    )
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Positioned(
+          top: 940,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Text(
+              'Акулов Р.В. ЭФБО-04-22',
+              style: TextStyle(
+                  color: Color(0xFFAA00AA),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
